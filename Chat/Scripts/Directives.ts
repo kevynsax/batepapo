ebbb0b44ba33095a5246af5c866911b2b6cf527b
kevyn pinheiro klava <kevyn_sax@hotmail.com>
2018-03-02 20:55:38 -0300

    Tentativa de salvar as paradas

---
+++ Chat/Scripts/Directives.ts
@@ -0,0 +1,13 @@
+﻿module Chat.Intranet.Directives {
+    export class MyEnter {
+        public link: (scope: ng.IScope, element: ng.IAugmentedJQuery, attrs: ng.IAttributes) => void;
+        public template = '<div>{{name}}</div>';
+        public scope = {};
+
+        constructor() {
+            MyEnter.prototype.link = (scope: ng.IScope, element: ng.IAugmentedJQuery, attrs: ng.IAttributes) => {
+
+            }https://stackoverflow.com/questions/17470790/how-to-use-a-keypress-event-in-angularjs
+        }
+    }
+}
